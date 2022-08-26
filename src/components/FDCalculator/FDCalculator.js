91b1882b0804b8d74ce00c9f4cdf7fd8f62c8864
Sharma, Pulkit K <pulkits1998@gmail.com>
2022-08-26 15:31:05 +0530

    Update FDCalculator.js

--- src/components/FDCalculator/FDCalculator.js
+++ src/components/FDCalculator/FDCalculator.js
@@ -32,7 +32,8 @@ const FDCalculator = () => {
   const handleSubmit = event => {
     event.preventDefault();
 
-    setInterestTable([]);
+    setInterestTable(new Array());
+    setDisplayResult(false);
 
     let interestCalculated = calculateInterest();
 
@@ -46,6 +47,7 @@ const FDCalculator = () => {
 
   const handleReset = event => {
     setDisplayResult(false);
+    setInterestTable([]);
   }
 
   function isLeapYear(year){
@@ -82,16 +84,53 @@ const FDCalculator = () => {
       return nextJanQ;
   }
 
-  function calculateNextQuarterReinvestment(date){
+  function calculateNextInterestDepositDate(date){
     const idate = new Date(date);
-    const nextQuarterDate = new Date(idate.setMonth(idate.getMonth()+3));
-    return nextQuarterDate;
+    idate.setMonth(idate.getMonth()+3);
+    idate.setDate(idate.getDate()-1);
+    return idate;
+  }
+
+  function calculateNextInterestDepositPrintDate(date){
+    const idate = new Date(date);
+    idate.setDate(idate.getDate+1);
+    return idate;
+  }
+
+  function calculateMonthEndDate(date){
+    
+    const thirtydaysMonths = [4,6,9,11];
+    const thirtyOnedaysMonths = [1,3,5,7,8,10,12];
+    const exceptionalMonth = [2];
+    const idate = new Date(date);
+    let monthNumber = idate.getMonth() + 1;
+    if(thirtydaysMonths.includes(monthNumber)){
+      idate.setDate(30);
+      return idate;
+    }
+    else if(thirtyOnedaysMonths.includes(monthNumber)){
+      idate.setDate(31);
+      return idate;
+    }
+    else if(exceptionalMonth.includes(monthNumber)){
+      if(isLeapYear(idate.getFullYear())){
+        idate.setDate(29);
+      }
+      else{
+        idate.setDate(28);
+      }
+      return idate;
+    }
+  }
+
+  function getNextDate(date){
+    const idate = new Date(date);
+    idate.setDate(idate.getDate() + 1);
+    return idate;
   }
   
 
   function calculateInterest() {
-
-    setInterestTable([]);
 
     let xamount = parseFloat(amount.current.value);
     let xroi = parseFloat(roi.current.value);
@@ -102,12 +141,12 @@ const FDCalculator = () => {
 
     console.log("Type of Deposit : " + typeOfDeposit);
 
-    const dom = new Date(xdod);
-    dom.setFullYear(dom.getFullYear() + xyears);
-    dom.setMonth(dom.getMonth() + xmonths);
-    dom.setDate(dom.getDate() + xdays);
+    const dateOfMaturity = new Date(xdod);
+    dateOfMaturity.setFullYear(dateOfMaturity.getFullYear() + xyears);
+    dateOfMaturity.setMonth(dateOfMaturity.getMonth() + xmonths);
+    dateOfMaturity.setDate(dateOfMaturity.getDate() + xdays);
 
-    console.log("Date of Maturity : " + dom.toDateString());
+    console.log("Date of Maturity : " + dateOfMaturity.toDateString());
 
     let output = {
       investedAmount : 0,
@@ -118,41 +157,82 @@ const FDCalculator = () => {
     if(typeOfDeposit === 'Reinvestment'){
 
       let currentDate = new Date(xdod);
-
+      let lastInterestDepositDate = currentDate;
       let currentAmount = parseFloat(xamount);
 
-      while(currentDate < dom){
+      let counter = 1;
+
+      let interestTableEntry = {
+        sno : counter,
+        date : currentDate.toDateString(),
+        interestAmount : 0,
+        interestCapitalized : 0,
+        totalAmount : currentAmount
+      }
+      interestTable.push(interestTableEntry);
+      setInterestTable(interestTable);
+
+      let date = "2022-05-23";
+      console.log(calculateMonthEndDate(date));
+
+      date = "2022-10-30";
+      console.log(calculateMonthEndDate(date));
+
+      date = "2022-06-25";
+      console.log(calculateMonthEndDate(date));
+
+      let interestAccumulatedButNotCredited = parseFloat(0);
+      while(currentDate < dateOfMaturity){
+        counter++;
+        console.log("Last Deposit Date : " + lastInterestDepositDate.toDateString());
         console.log("Current Date : " + currentDate.toDateString());
         let currentYear = currentDate.getFullYear();
         let yearlyInterest = currentAmount * xroi * 0.01;
         console.log("Yearly Interest : " + yearlyInterest);
         let perDayInterest = (currentAmount * xroi * 0.01)/(isLeapYear(currentYear)?366:365);
         console.log("Per Day Interest : " + perDayInterest);
-        let nextQuarterDate = calculateNextQuarterReinvestment(currentDate);
-        let remainingNoOfDays = 0;
-        if(nextQuarterDate > dom){
-          nextQuarterDate = dom;
-          remainingNoOfDays = Math.ceil(Math.abs(nextQuarterDate - currentDate)/(1000 * 60 * 60 * 24));
-          console.log("Number of Days : " + remainingNoOfDays);
+        let interestCalculationDate = calculateMonthEndDate(currentDate);
+        let interestDepositDate = calculateNextInterestDepositDate(lastInterestDepositDate);
+        if(interestDepositDate > dateOfMaturity){
+          interestDepositDate = dateOfMaturity;
         }
-        console.log("Next Quarter Date : " + nextQuarterDate.toDateString());
+        console.log("Interest Deposit Date : " + interestDepositDate.toDateString());
+        let noOfDays = 0;
+        let interestCapitalized = 0;
+        if(interestCalculationDate > interestDepositDate){
+          interestCalculationDate = interestDepositDate;
+        }
+        console.log("Interest Calculation Date : " + interestCalculationDate.toDateString());
+
+        noOfDays = Math.ceil(Math.abs(interestCalculationDate - currentDate + 1)/(1000 * 60 * 60 * 24));
+        // if(interestCalculationDate === interestDepositDate){
+        //   noOfDays = Math.ceil(Math.abs(interestCalculationDate - currentDate)/(1000 * 60 * 60 * 24));
+        // }
+        console.log("Number of Days : " + noOfDays);
         
-        let interest = yearlyInterest * 0.25;
-        if(remainingNoOfDays != 0){
-          interest = remainingNoOfDays * perDayInterest;
-        }
+        let interest = noOfDays * perDayInterest;
         interest = interest.toFixed(2);
-        console.log("Interest : " + interest);
-        currentAmount = currentAmount + parseFloat(interest);
+        interestAccumulatedButNotCredited += parseFloat(interest);
+        console.log("Interest For This Month : " + interest);
+        console.log("Interest Accumulated But Not Credited : " + interestAccumulatedButNotCredited);
+        
+        if(interestCalculationDate === interestDepositDate){
+          interestCapitalized = interestAccumulatedButNotCredited.toFixed(0);
+          currentAmount = currentAmount + parseFloat(interestCapitalized);
+          lastInterestDepositDate = interestDepositDate;
+          interestAccumulatedButNotCredited = 0;
+        }
         console.log("Current Amount : " + currentAmount);
         let interestTableEntry = {
-          date : nextQuarterDate.toDateString(),
+          sno : counter,
+          date : interestCalculationDate.toDateString(),
           interestAmount : interest,
-          totalAmount : currentAmount
+          interestCapitalized : interestCapitalized,
+          totalAmount : currentAmount.toFixed(0)
         }
         interestTable.push(interestTableEntry);
         setInterestTable(interestTable);
-        currentDate = nextQuarterDate;
+        currentDate = getNextDate(interestCalculationDate);
       }
 
       output.totalAmount = currentAmount;
@@ -175,28 +255,28 @@ const FDCalculator = () => {
   )
 
   const ResultTable = () => (
-    <Container>
         <Table striped bordered hover size="sm">
           <thead>
             <tr>
               <th>#</th>
               <th>Date</th>
               <th>Interest Amount</th>
+              <th>Interest Capitalized</th>
               <th>Total Amount</th>
             </tr>
           </thead>
           <tbody>
             {interestTable.map((element) => (
               <tr>
-                <td></td>
+                <td>{element.sno}</td>
                 <td>{element.date}</td>
                 <td>{element.interestAmount}</td>
+                <td>{element.interestCapitalized}</td>
                 <td>{element.totalAmount}</td>
             </tr>
             ))}
           </tbody>
         </Table>
-      </Container>
   )
 
   return (
@@ -263,8 +343,9 @@ const FDCalculator = () => {
         </Form>
 
         { displayResult && <Results/> }
+        { displayResult && <ResultTable/> }
       </Container>
-      { displayResult && <ResultTable/> }
+      
 
     </div>
   )
